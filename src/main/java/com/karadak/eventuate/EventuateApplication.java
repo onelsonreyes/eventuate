1d76720da17ec8a0132503c45624023cdaa5e884
onelsonreyes <onelson.reyes@gmail.com>
2017-06-12 16:13:30 -0600

    added initial configuration
    added spring boot project configuration

---
+++ src/main/java/com/karadak/eventuate/EventuateApplication.java
@@ -0,0 +1,12 @@
+package com.karadak.eventuate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventuateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventuateApplication.class, args);
+	}
+}
---
+++ src/test/java/com/karadak/eventuate/EventuateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.karadak.eventuate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventuateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
